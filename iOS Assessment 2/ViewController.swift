347f600238f92a109f449bb6e9c679c3729d5aa7
Gabriella Torres <gabrielatorres@Interapts-MacBook-Pro-6.local>
2018-10-25 09:59:54 -0500

    Initial Commit

---
+++ iOS Assessment 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Assessment 2
+//
+//  Created by Gabriela Torres on 10/25/18.
+//  Copyright © 2018 Gabriela Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
